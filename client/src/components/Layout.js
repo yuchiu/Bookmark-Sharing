8c542cf3793e57d6feb55833e201ab517b83629d
yu <yuchiu2002@hotmail.com>
2017-12-13 00:22:41 -0500

    initialize client side directory

---
+++ client/src/components/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../../assets/scss/styles.scss'
+
+class Layout extends React.Component {
+
+  render() {
+    return (
+      <div id="title">
+        My React Boilerplate
+      </div>
+
+    )
+  }
+}
+
+export default Layout;
